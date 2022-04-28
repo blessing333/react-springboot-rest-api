e37816a7a1521d3ca3f8e3b51dcb5f7963c2cf9e
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-04-28 16:34:41 +0900

    git init

---
+++ src/test/java/com/blessing333/gccoffee/GcCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blessing333.gccoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
