33c2c8039801516a790e7b2cc41a955bad6a03fb
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-05-01 17:58:16 +0900

    feat : OrderRepository

---
+++ src/main/java/com/blessing333/gccoffee/domain/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.blessing333.gccoffee.domain.repository;
+
+import com.blessing333.gccoffee.domain.Order;
+
+public interface OrderRepository {
+    Order insert(Order order);
+}
---
+++ src/main/java/com/blessing333/gccoffee/web/api/ProductRestController.java
@@ -0,0 +1,27 @@
+package com.blessing333.gccoffee.web.api;
+
+import com.blessing333.gccoffee.domain.Category;
+import com.blessing333.gccoffee.domain.Product;
+import com.blessing333.gccoffee.domain.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductRestController {
+    private final ProductService productService;
+
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/api/v1/products")
+    public List<Product> productList(@RequestParam Optional<Category> category) {
+        return category
+                .map(productService::getProductsByCategory)
+                .orElse(productService.getAllProducts());
+    }
+}
